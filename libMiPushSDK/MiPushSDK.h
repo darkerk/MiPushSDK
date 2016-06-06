4c20cf5d4557ab8ab73f534121d0d0fa43eb5bf3
wgh <appwgh@gmail.com>
2016-06-06 12:39:06 +0800

    fix UIRemoteNotificationType ios8 bulid warning

--- libMiPushSDK/MiPushSDK.h
+++ libMiPushSDK/MiPushSDK.h
@@ -47,8 +47,8 @@
  *      connect: 是否启动长连接, 它跟APNSs是不同的通道(不管是否启动系统推送, app在前台都可以收到在线或离线消息)
  */
 + (void)registerMiPush:(id<MiPushSDKDelegate>)delegate;
-+ (void)registerMiPush:(id<MiPushSDKDelegate>)delegate type:(UIRemoteNotificationType)type;
-+ (void)registerMiPush:(id<MiPushSDKDelegate>)delegate type:(UIRemoteNotificationType)type connect:(BOOL)connect;
++ (void)registerMiPush:(id<MiPushSDKDelegate>)delegate type:(UIUserNotificationType)type;
++ (void)registerMiPush:(id<MiPushSDKDelegate>)delegate type:(UIUserNotificationType)type connect:(BOOL)connect;
 
 /**
  * 客户端设备注销
